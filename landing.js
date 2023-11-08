9115cca4490b778e44236c72575970fe342544c4
senarchi4@gmail.com <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>
2023-11-08 15:25:24 +0530

    language

--- landing.js
+++ landing.js
@@ -1,8 +1,12 @@
 const btn = document.querySelector(".btn");
 const text=document.querySelector(".text");
+const speech = document.querySelector(".speak");
+const language = document.getElementById('languageSelector');
 document.addEventListener('DOMContentLoaded',getJoke);
 
-btn.addEventListener("click",getJoke)
+btn.addEventListener("click",getJoke);
+speech.addEventListener('click',speakJoke);
+language.addEventListener('change',updateText);
 
 
 async function getJoke(){
@@ -15,4 +19,30 @@ async function getJoke(){
     const formatData=await data.json();
     console.log(formatData);
     text.innerHTML=formatData.joke;
+}
+
+function speakJoke(){
+    const joke= text.innerHTML;
+    const selectedLanguage= language.value;
+    console.log(selectedLanguage);
+    console.log(joke);
+    const speech = new SpeechSynthesisUtterance();
+    speech.text= joke;
+    speech.lang= selectedLanguage;
+
+    const voices= window.speechSynthesis.getVoices();
+    const voiceForLanguage= voices.find(voice=> voice.lang == selectedLanguage);
+    if(voiceForLanguage){
+        console.log('Yes');
+        speech.voice = voiceForLanguage;
+    }
+    console.log(voices);
+    speech.voices= voices[0];
+    window.speechSynthesis.speak(speech);
+}
+
+function updateText(){
+    const selectedLanguageText= language.value;
+    console.log('selectedLanguageText',selectedLanguageText)
+    text.lang = selectedLanguageText;
 }
